4d7d91e158c3e0c82d7fdf470b1726c806f942f3
Zachary Di <zacharysiefkes@gmail.com>
2021-01-08 16:40:04 +1300

    Add webview and display progress bar, page title and icon

--- app/src/main/java/com/dizach/androidbrowser/MainActivity.java
+++ app/src/main/java/com/dizach/androidbrowser/MainActivity.java
@@ -2,13 +2,68 @@ package com.dizach.androidbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    ImageView iconImageView;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        iconImageView = findViewById(R.id.iconImageView);
+        webView = findViewById(R.id.webViewMain);
+
+        progressBar.setMax(100);
+
+        webView.loadUrl("https://www.google.com");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // use webchromeclient to add some cool features
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                // update progress bar
+                progressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                // display page title
+                getSupportActionBar().setTitle(title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                // display page icon
+                iconImageView.setImageBitmap(icon);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // close app if back button is pressed but there's no previous page in history
+            finish();
+        }
     }
 }
