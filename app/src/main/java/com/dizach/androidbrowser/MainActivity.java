40cfe70664debb07f8f72d7c5a01ba6c8fc3a025
Zachary Di <zacharysiefkes@gmail.com>
2021-01-10 14:43:35 +1300

    Add URL bar

--- app/src/main/java/com/dizach/androidbrowser/MainActivity.java
+++ app/src/main/java/com/dizach/androidbrowser/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +17,8 @@ import android.webkit.DownloadListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -27,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView iconImageView;
     WebView webView;
     LinearLayout linearLayoutProgressIcon;
+    EditText URLBar;
+    Button goButton;
     String currentURL;
+    String homePage = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +44,12 @@ public class MainActivity extends AppCompatActivity {
         iconImageView = findViewById(R.id.iconImageView);
         webView = findViewById(R.id.webViewMain);
         linearLayoutProgressIcon = findViewById(R.id.linearLayoutProgressIcon);
+        URLBar = findViewById(R.id.editTextURL);
+        goButton = findViewById(R.id.goButton);
 
         progressBar.setMax(100);
 
-        webView.loadUrl("https://www.google.com");
+        webView.loadUrl(homePage);
         webView.getSettings().setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient(){
@@ -54,9 +62,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
                 // hide progress bar once loading is finished
                 linearLayoutProgressIcon.setVisibility(View.GONE);
-                super.onPageFinished(view, url);
+
+                // update address in URL bar
+                URLBar.setText(url);
                 currentURL = url;
             }
         });
@@ -91,13 +102,32 @@ public class MainActivity extends AppCompatActivity {
             public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
 
                 DownloadManager.Request downloadRequest = new DownloadManager.Request(Uri.parse(url));
-                downloadRequest.allowScanningByMediaScanner();
+//                downloadRequest.allowScanningByMediaScanner();
                 downloadRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 
                 DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
                 downloadManager.enqueue(downloadRequest);
 
                 Toast.makeText(MainActivity.this, "File is downloading", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // set go button event listener
+        goButton.setOnClickListener((View v) -> {
+            String toURL = URLBar.getText().toString();
+            webView.loadUrl(toURL);
+        });
+        // want to go to page if user hits enter on onscreen keyboard
+        URLBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    String toURL = URLBar.getText().toString();
+                    webView.loadUrl(toURL);
+                    return true;
+                } else {
+                    return false;
+                }
             }
         });
     }
